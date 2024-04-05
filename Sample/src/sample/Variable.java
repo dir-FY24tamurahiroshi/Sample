5577aaa0c1692c9f9330316fad7f5de556d095f0
HiroshiTamura(田村博) <HiroshiTamura()@Dirbato-1360x>
2024-04-05 17:33:26 +0900

    初日のコミットです

---
+++ Sample/src/sample/Variable.java
@@ -0,0 +1,17 @@
+package sample;
+
+import javax.swing.AbstractCellEditor;
+
+public class Variable {
+
+    public static void main(String[] args) {
+        var number1 = 1;
+        System.out.println(number1);
+        
+        
+        String str1;
+        str1 = "abc" ;
+        System.out.println(str1);
+    }
+
+}
